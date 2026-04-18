7cf00b65a4418f0888fcbc2d1e9da4810fcb5452
srilalita-05 <srilalitadevarakonda@gmail.com>
2026-04-18 19:51:52 +0530

    updated

---
+++ reset_admin.py
@@ -0,0 +1,25 @@
+import os
+from werkzeug.security import generate_password_hash
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conn = psycopg2.connect(os.environ['DATABASE_URL'])
+cur = conn.cursor()
+
+new_password = 'bhuvana@123'
+new_hash = generate_password_hash(new_password)
+
+cur.execute(
+    "UPDATE users SET password_hash = %s WHERE username = 'kavitha'",
+    (new_hash,)
+)
+
+conn.commit()
+
+cur.execute("SELECT username, role FROM users WHERE username = 'kavitha'")
+print("Updated:", cur.fetchone())
+
+cur.close()
+conn.close()
